3bb130240a7cfc9528c6fe8b95765773201822ea
Nouman Gul <Noumanguljunejo@gmail.com>
2024-07-09 10:09:30 +0400

    Introduced LevitatingViewModel to manage the dynamic behavior of UI components.

---
+++ LevitateView/LevitatingView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  LevitateView
+//
+//  Created by Nouman Gul Junejo on 09/07/2024.
+//
+
+import SwiftUI
+
+struct LevitatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevitatingView()
+}
---
+++ LevitateView/Model/LevitatingViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  LevitatingViewModel.swift
+//  LevitateView
+//
+//  Created by Nouman Gul Junejo on 09/07/2024.
+//
+
+import SwiftUI
+
+public struct LevitatingViewModel {
+    public var image: Image
+    public var levitateBottomPadding: CGFloat
+    public var levitateScrollOffset: CGFloat
+    public var colorBackground: Color
+    public var colorForeground: Color
+
+    // Initializer with default values
+    public init(image: Image = Image(systemName: "arrow.up"),
+                levitateBottomPadding: CGFloat = 0,
+                levitateScrollOffset: CGFloat = 0,
+                colorBackground: Color = .red,
+                colorForeground: Color = .white) {
+        self.image = image
+        self.levitateBottomPadding = levitateBottomPadding
+        self.levitateScrollOffset = levitateScrollOffset
+        self.colorBackground = colorBackground
+        self.colorForeground = colorForeground
+    }
+}
