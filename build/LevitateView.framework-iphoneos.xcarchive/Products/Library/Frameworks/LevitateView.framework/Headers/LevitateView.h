5b210ea215105b700b7026bd30cde527945e061c
Nouman Gul <Noumanguljunejo@gmail.com>
2024-07-09 21:11:07 +0400

    adding version

---
+++ build/LevitateView.framework-iphoneos.xcarchive/Products/Library/Frameworks/LevitateView.framework/Headers/LevitateView.h
@@ -0,0 +1,18 @@
+//
+//  LevitateView.h
+//  LevitateView
+//
+//  Created by Nouman Gul Junejo on 09/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LevitateView.
+FOUNDATION_EXPORT double LevitateViewVersionNumber;
+
+//! Project version string for LevitateView.
+FOUNDATION_EXPORT const unsigned char LevitateViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LevitateView/PublicHeader.h>
+
+
